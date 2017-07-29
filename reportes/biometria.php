ce685cd9a20fb201937fb7461da9a918361c3879
edev <erwinXZ@hotmail.com>
2017-07-29 10:28:11 -0400

    cambios reportes

--- reportes/biometria.php
+++ reportes/biometria.php
@@ -42,9 +42,9 @@ $pdf->AddPage();
 // $pdf->SetFont('helvetica', 'B', 9);
 $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 $pdf->SetFillColor(59,78,20);
-$pdf->SetTextColor(0,0,0);
-$image_file = K_PATH_IMAGES.'logovosandes.jpg';
+$pdf->SetTextColor(37,65,98);
 
+$image_file = K_PATH_IMAGES.'logovosandes.jpg';
 $pdf->Image($image_file, 30, 2, 20, '', 'JPG', '', 'T', false, 200, '', false, false, 0, false, false, false);
 $pdf->Ln(3);
 // $pdf->Cell(0, 15, '                         LABORATORIO DE ANÁLISIS CLÍNICO "VOS ANDES"', 0, false, 'L', 0, '', 0, false, 'M', 'M');
@@ -52,11 +52,12 @@ $titleP = '<p><b>LABORATORIO DE ANÁLISIS CLÍNICO</b> <b style="color: rgb(150,
 $pdf->writeHTML($titleP, true, false, false, false, 'C');
 $pdf->Ln(2);
 $pdf->SetFont('helvetica', '', 11);
-$pdf->Cell(0, 15, '                                  Dir.: Av. Camacho esq. Oruro Clínica 1º de mayo', 0, false, 'L', 0, '', 0, false, 'M', 'M');
+$pdf->Cell(0, 15, '                                  Dir.: Av. Camacho esq. Oruro Clínica 1º de mayo        Telf.: 62-23510', 0, false, 'L', 0, '', 0, false, 'M', 'M');
 $pdf->Ln(5);
-$pdf->Cell(0, 15, '                                  Telf.: 62-23510        Cel.: 72414698        Emergencias las 24 horas.', 0, false, 'L', 0, '', 0, false, 'M', 'M');
+$pdf->Cell(0, 15, '                                  Cel.: 72414698        E-mail: labvosandes@gmail.com        Emergencias las 24 horas.', 0, false, 'L', 0, '', 0, false, 'M', 'M');
 
 $style = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(31, 77, 120));
+$pdf->Line(152.5, 19.5, 199, 19.5, $style);
 $pdf->Line(2, 22, 212, 22, $style);
 
 // set alpha to semi-transparency
@@ -69,15 +70,15 @@ $pdf->Ln(8);
 foreach ($rowp as $rows1){
 $initData = '<table>
                 <tr>
-                    <td>Nombre: '.$rows1[0].' '.$rows1[1].'</td>
-                    <td>Edad: '.$rows1[2].'</td>
+                    <td><p><FONT style="color: rgb(150,0,0)">Nombre: </FONT>'.$rows1[0].' '.$rows1[1].'</p></td>
+                    <td><p><FONT style="color: rgb(150,0,0)">Edad: </FONT>'.$rows1[2].'</p></td>
                 </tr>
                 <tr>
-                    <td>Dr.(a): '.$rows1[3].' '.$rows1[4].'</td>
-                    <td>Fecha: '.$rows1[5].'</td>
+                    <td><p><FONT style="color: rgb(150,0,0)">Dr.(a): </FONT>'.$rows1[3].' '.$rows1[4].'</p></td>
+                    <td><p><FONT style="color: rgb(150,0,0)">Fecha: </FONT>'.$rows1[5].'</p></td>
                 </tr>
             </table>'; 
-    $nombre = 'Biometria_'.$rows1[0].'_'.$rows1[1]; 
+    $nombre = 'GeneralO_'.$rows1[0].'_'.$rows1[1]; 
 }
 $pdf->writeHTMLCell($w=180, $h=0, $x='40', $y='', utf8_encode($initData), $border=0, $ln=1, $fill=0, $reseth=true, $align='L', $autopadding=true);
 
@@ -94,23 +95,23 @@ $tabla1 = '<table>
                 </tr>
                 <tr>
                     <td>Hematíes: '.$rows[1].' mm3</td>
-                    <td>H: 5`2 - 5`6; M: 5`2 - 5`4</td>
+                    <td style="color: rgb(58,137,159)">H: 5`2 - 5`6; M: 5`2 - 5`4</td>
                 </tr>
                 <tr>
                     <td>Hematocrito: '.$rows[2].' %</td>
-                    <td>H: 49 - 53; M: 47 - 51 </td>
+                    <td style="color: rgb(58,137,159)">H: 49 - 53; M: 47 - 51 </td>
                 </tr>
                 <tr>
                     <td>Hemoglobina: '.$rows[3].' gr/dl</td>
-                    <td>H: 16 +/- 1,5; M: 15 +/- 0,5</td>
+                    <td style="color: rgb(58,137,159)">H: 16 +/- 1,5; M: 15 +/- 0,5</td>
                 </tr>
                 <tr>
                     <td>Leucocito: '.$rows[4].' mm3</td>
-                    <td>5,000 - 8,000 </td>
+                    <td style="color: rgb(58,137,159)">5,000 - 8,000 </td>
                 </tr>
                 <tr>
                     <td>V. S. G.: '.$rows[5].' mm/hra</td>
-                    <td>1 - 10</td>
+                    <td style="color: rgb(58,137,159)">1 - 10</td>
                 </tr>
                 <tr>
                     <br>
@@ -119,19 +120,19 @@ $tabla1 = '<table>
                 </tr>
                 <tr>
                     <td>V. C. M.: '.$rows[6].' fl.</td>
-                    <td>90 +/- 8</td>
+                    <td style="color: rgb(58,137,159)">90 +/- 8</td>
                 </tr>
                 <tr>
                     <td>Hb. C. M.: '.$rows[7].' pg.</td>
-                    <td>30 +/- 3</td>
+                    <td style="color: rgb(58,137,159)">30 +/- 3</td>
                 </tr>
                 <tr>
                     <td>C. Hb. C. M.: '.$rows[8].' %</td>
-                    <td>34 +/- 2</td>
+                    <td style="color: rgb(58,137,159)">34 +/- 2</td>
                 </tr>
                 <tr>
                     <td colspan="2">Comentario: '.$rows[9].'</td>
-                    <td></td>
+                    <td style="color: rgb(58,137,159)"></td>
                 </tr>
 </table>';
 $tabla2 = '<table>
@@ -141,27 +142,27 @@ $tabla2 = '<table>
                 </tr>
                 <tr>
                     <td>Cayados: '.$rows[10].'</td>
-                    <td>1 - 5 %</td>
+                    <td style="color: rgb(58,137,159)">1 - 5 %</td>
                 </tr>
                 <tr>
                     <td>Neutrófilos: '.$rows[11].'</td>
-                    <td>50 - 70 %</td>
+                    <td style="color: rgb(58,137,159)">50 - 70 %</td>
                 </tr>
                 <tr>
                     <td>Basófilo: '.$rows[12].'</td>
-                    <td>0 - 1 %</td>
+                    <td style="color: rgb(58,137,159)">0 - 1 %</td>
                 </tr>
                 <tr>
                     <td>Eosinófilo: '.$rows[13].'</td>
-                    <td>1 - 3 %</td>
+                    <td style="color: rgb(58,137,159)">1 - 3 %</td>
                 </tr>
                 <tr>
                     <td>Linfocito: '.$rows[14].'</td>
-                    <td>25 - 35 %</td>
+                    <td style="color: rgb(58,137,159)">25 - 35 %</td>
                 </tr>
                 <tr>
                     <td>Monocito: '.$rows[15].'</td>
-                    <td>4 - 8 %</td>
+                    <td style="color: rgb(58,137,159)">4 - 8 %</td>
                 </tr>
                 <tr>
                     <td>Pro linfocito: '.$rows[16].'</td>
