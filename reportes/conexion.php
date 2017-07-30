8ff180a4812c2286e2b9a0d777207bf67c67e159
edev <erwinXZ@hotmail.com>
2017-07-30 11:34:21 -0400

    Bug de tildes y ñ

--- reportes/conexion.php
+++ reportes/conexion.php
@@ -6,7 +6,8 @@ function Conectar () {
     $user = 'root';
     $pwd = '';
     try{
-        $conn = new PDO('mysql:host='.$host.';dbname='.$db, $user, $pwd);
+        $conn = new PDO('mysql:host='.$host.';dbname='.$db, $user, $pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES  \'UTF8\''));
+        
     }catch (PDOException $e) {
         echo '<p>No se puede conectar a la base de datos!</p>';
         exit;
